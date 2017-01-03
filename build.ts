3c9ba7b9ba31a3933870354431e60458451b457d
Dan Stephenson <ispyhumanfly@gmail.com>
2017-01-03 01:20:52 +0000

    Added beautification to the bundled distribution.

--- build.ts
+++ build.ts
@@ -7,6 +7,6 @@ shell.exec("node_modules/.bin/tsc src/x-tag-autoscroll.ts")
 shell.exec("node_modules/.bin/uglifyjs -b -o dist/x-tag-autoscroll.js src/x-tag-autoscroll.js")
 shell.exec("node_modules/.bin/uglifyjs -o dist/x-tag-autoscroll.min.js src/x-tag-autoscroll.js")
 shell.rm("src/x-tag-autoscroll.js")
-shell.exec("node_modules/.bin/browserify -o dist/x-tag-autoscroll.bundle.js dist/x-tag-autoscroll.js")
+shell.exec("node_modules/.bin/browserify -b -o dist/x-tag-autoscroll.bundle.js dist/x-tag-autoscroll.js")
 shell.exec("node_modules/.bin/uglifyjs -o dist/x-tag-autoscroll.bundle.min.js dist/x-tag-autoscroll.bundle.js")
 
